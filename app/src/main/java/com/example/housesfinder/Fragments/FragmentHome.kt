b38ebcc01196a906179b207ba012cb27a0f979c0
SihemBouhenniche <fs_bouhenniche@esi.dz>
2019-08-15 14:57:31 +0200

    #2.1 : add google authentification

--- app/src/main/java/com/example/housesfinder/Fragments/FragmentHome.kt
+++ app/src/main/java/com/example/housesfinder/Fragments/FragmentHome.kt
@@ -1,5 +1,6 @@
 package com.example.housesfinder.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,17 +11,40 @@ import com.example.housesfinder.Adapters.AnnonceAdapter
 import com.example.housesfinder.R
 import kotlinx.android.synthetic.main.fragment_home.*
 import androidx.appcompat.widget.PopupMenu
+import com.example.housesfinder.Activities.WelcomeSplashActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class FragmentHome : Fragment() {
 
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     var adapter: AnnonceAdapter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         adapter = AnnonceAdapter(MainActivity.listAnnoce, this!!.activity!!)
+        // [START config_signin]
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        // [END config_signin]
 
+        googleSignInClient = GoogleSignIn.getClient(context!!, gso)
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -31,10 +55,8 @@ class FragmentHome : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         annoncesList.adapter = adapter
-        more_btn.setOnClickListener {
-            val menu = PopupMenu(context!!, it)
-            menu.getMenu().add("Ajouter wilaya préféré")
-            menu.show()
+        logout_btn.setOnClickListener {
+            signOut()
         }
         notification_btn.setOnClickListener {
             val fragment = FragmentNotifications()
@@ -52,5 +74,25 @@ class FragmentHome : Fragment() {
             .replace(R.id.frame_container, fragment, fragment.javaClass.getSimpleName())
             .addToBackStack(fragment.javaClass.getSimpleName())
             .commit()
+    }
+
+    private fun signOut() {
+        //verify first if we are using google or facebook account then deconnect
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(activity!!) {
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+        if (user != null) {
+            startActivity(Intent(activity, MainActivity::class.java))
+        } else {
+            startActivity(Intent(activity,WelcomeSplashActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/housesfinder/Fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/housesfinder/Fragments/RegisterFragment.kt
@@ -2,6 +2,7 @@ package com.example.housesfinder.Fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +11,33 @@ import androidx.fragment.app.Fragment
 import com.example.housesfinder.Activities.MainActivity
 import com.example.housesfinder.R
 import com.example.housesfinder.Model.Seller
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
+    //Google Login Request Code
+    private val RC_SIGN_IN = 7
+    //Google Sign In Client
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    //Firebase Auth
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MainActivity.mainSeller = Seller(
+            "Sihem",
+            "Bouhenniche",
+            "0551789142",
+            "fs@esi.dz"
+        )
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -24,26 +47,79 @@ class RegisterFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("810538588808-dccn6dg1ncs6oe30rt3r2sms6ck4h3is.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(context!!,gso)
 
         google_btn.setOnClickListener {
-            MainActivity.mainSeller = Seller(
-                "Sihem",
-                "Bouhenniche",
-                "0551789142",
-                "fs@esi.dz"
-            )
-            startActivity(Intent(activity, MainActivity::class.java))
+            signIn()
         }
         facebook_btn.setOnClickListener {
-            MainActivity.mainSeller = Seller(
-                "Sihem",
-                "Bouhenniche",
-                "0551789142",
-                "fs@esi.dz"
-            )
             startActivity(Intent(activity, MainActivity::class.java))
         }
 
     }
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.getSignInIntent()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("Login", "Google sign in failed", e)
+                // ...
+            }
+
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d("Login", "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(activity!!) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Login", "signInWithCredential:success")
+                    val user = mAuth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Login", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(context!!,"Auth Failed",Toast.LENGTH_LONG).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(activity, MainActivity::class.java))
+        }
+    }
 }
